84b7876d942b6990760266af83fb0e8aebbe71b3
Rainer <rainermr89@hotmail.com>
2022-03-25 10:36:32 -0300

    Propriedade Length e sobrecargas de IndexOf

--- ByteBank.SistemaAgencia/ExtratorValorDeArgumentosURL.cs
+++ ByteBank.SistemaAgencia/ExtratorValorDeArgumentosURL.cs
@@ -4,25 +4,28 @@ namespace ByteBank.SistemaAgencia
 {
     public class ExtratorValorDeArgumentosURL
     {
+        private readonly string _argumentos;
         public string URL { get; }
 
         public ExtratorValorDeArgumentosURL(string url)
         {
-            string.IsNullOrEmpty(url);
-
-
-
-            if (url == null)
+           if(string.IsNullOrEmpty(url))
             {
-                throw new ArgumentNullException(nameof(url));
+                throw new ArgumentException("O argumento url não pode ser nulo ou vazio", nameof(url));
             }
+                       
 
-            if(url == "")
-            {
-                throw new ArgumentException("O argumento url não pode ser uma string vazia", nameof(url));
-            }
+            int indiceInterrogacao = URL.IndexOf('?');
+            _argumentos = url.Substring(indiceInterrogacao + 1);
 
             URL = url;
+        }
+
+        // moedaOrigem=real&moedaDestino=dolar
+        public string GetValor(string nomeParametro)
+        {
+            return "";
+           // int indiceParametro = _argumentos.IndexOf
         }
     }
 }
